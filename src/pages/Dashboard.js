41767cf25442678faffa58bbef06bf8b5ba78073
M LavaT <mwhlavaty@gmail.com>
2023-07-30 15:44:57 -0400

    work on login, dashboard, firebase connection

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,4 +1,65 @@
 import React, { useRef, useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import { db, auth } from "../services/firebaseConnection";
+import {
+  doc,
+  addDoc,
+  setDoc,
+  getDoc,
+  getDocs,
+  updateDoc,
+  collection,
+} from "firebase/firestore";
 
-export default function Dashboard() { }
+export default function Home() {
+  useEffect(() => {
+    // "document.documentElement.scrollTo" is the magic for React Router Dom v6
+    document.documentElement.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "instant", // Optional if you want to skip the scrolling animation
+    });
+  }, []);
+
+  const [allPosts, setAllPosts] = useState([]);
+
+  useEffect(() => {
+    const userAuth = auth.onAuthStateChanged(async (authUser) => {
+      if (authUser) {
+        console.log(authUser);
+        let querySnapshot = await getDocs(collection(db, "posts"));
+        querySnapshot.forEach((doc) => {
+          console.log(doc.data());
+          setAllPosts([...allPosts, doc.data()]);
+        });
+      } else if (!authUser) {
+        console.log("not signed in");
+      }
+      return userAuth;
+    });
+  }, []);
+
+  return (
+    <div className="bg-gray-100">
+      <main className="max-w-4xl mx-auto py-8 px-6">
+        <h1 className="text-4xl font-bold text-gray-900 mb-2">
+          Account Dashboard
+        </h1>
+        <div className="flex flex-row justify-start w-3/4">
+          <div className="text-lg text-gray-800 my-2">Account Email:</div>
+          <div className="text-lg text-gray-800 my-2 pl-4">
+            {/* {userInfo.email} */}
+          </div>
+        </div>
+
+        <div className="flex justify-center space-x-4">
+          <Link to="/locations">
+            <button className="bg-emerald-400 text-gray-800 hover:bg-emerald-600 font-semibold py-2 px-4 rounded-md drop-shadow-md">
+              Create New Blog Post
+            </button>
+          </Link>
+        </div>
+      </main>
+    </div>
+  );
+}
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,109 @@
-import React, { useRef, useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
+import { Form, Button } from "react-bootstrap";
+import Alert from "react-bootstrap/Alert";
+import { auth } from "../services/firebaseConnection";
+import { connectAuthEmulator, signInWithEmailAndPassword } from "firebase/auth";
 import { Link } from "react-router-dom";
 
-export default function Login() {}
+export default function Login() {
+  const alertDiv = document.getElementById("alert");
+
+  const [validated, setValidated] = useState(false);
+  const [show, setShow] = useState(false);
+  const [userFormData, setUserFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleSignupBtn = (event) => {
+    window.location.assign("/signup");
+  };
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setUserFormData({ ...userFormData, [name]: value });
+    console.log(userFormData);
+  };
+
+  const handleFormSubmit = async (event) => {
+    console.log("submit clicked");
+    event.preventDefault();
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+      setValidated(true);
+    } else {
+      // AUTHENTICATE USER SIGN-UP IN FIREBASE
+      try {
+        const userCredential = await signInWithEmailAndPassword(
+          auth,
+          userFormData.email,
+          userFormData.password
+        );
+        console.log(userCredential);
+        if (!userCredential) {
+          window.location.assign("/");
+        } else if (userCredential) {
+          window.location.assign("/dashboard");
+        }
+      } catch (error) {
+        if (error.code === "auth/user-not-found") {
+          setShow(true);
+        }
+      }
+    }
+  };
+
+  return (
+    <>
+      <Form
+        className="container mx-auto mt-6 p-4 w-3/4 bg-zinc-300 rounded-lg drop-shadow-xl"
+        noValidate
+        validated={validated}
+        onSubmit={handleFormSubmit}
+      >
+        <Alert onClose={() => setShow(false)} show={show} dismissible>
+          Something went wrong with your credentials!
+        </Alert>
+        <Form.Group className="flex flex-row justify-center py-4">
+          <Form.Label htmlFor="email">Email</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Your email"
+            name="email"
+            onChange={handleInputChange}
+            value={userFormData.email}
+            required
+          />
+          <Form.Control.Feedback type="invalid">
+            Email is required
+          </Form.Control.Feedback>
+        </Form.Group>
+
+        <Form.Group className="flex flex-row justify-center py-4">
+          <Form.Label htmlFor="password">Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Your password"
+            name="password"
+            onChange={handleInputChange}
+            value={userFormData.password}
+            required
+          />
+          <Form.Control.Feedback type="invalid">
+            Password is required!
+          </Form.Control.Feedback>
+        </Form.Group>
+        <br></br>
+        <button
+          className="container mx-auto w-1/2 py-2 px-4 bg-emerald-400 text-gray-800 hover:bg-emerald-800 font-semibold rounded-md drop-shadow-md"
+          type="submit"
+        >
+          Submit
+        </button>
+      </Form>
+      <br></br>
+    </>
+  );
+}
